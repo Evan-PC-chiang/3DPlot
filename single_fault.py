110ef7da5e4b2376ece4e52db38ef3bae6dfd4fe
Evan-PC-chiang <113721214+Evan-PC-chiang@users.noreply.github.com>
2023-09-06 16:44:54 -0400

    start

---
+++ single_fault.py
@@ -0,0 +1,5 @@
+from PlotFault import Fault, Costal, SetUp
+import numpy as np
+import plotly.graph_objects as go
+
+
---
+++ test_class.py
@@ -0,0 +1,17 @@
+from PlotFault import Fault, Costal, SetUp
+import plotly.graph_objects as go
+import pandas as pd
+
+
+costal_file = 'tri/ll_costal.txt'
+line = Costal(costal_file).plot()
+
+filenames = pd.read_table('tri/namelist.txt')
+surface_list = [line]
+
+for i in filenames.namelist:
+    surface_list.append(Fault(i).tri("Ld"))
+
+fig = go.Figure(data=surface_list)
+
+SetUp(surface_list).reg("locking", 'oranges', 'Ratio')

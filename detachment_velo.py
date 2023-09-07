8f7d77fe7e5100501f3c5241232eb8d9c1cc34cb
Evan-PC-chiang <113721214+Evan-PC-chiang@users.noreply.github.com>
2023-09-07 17:12:40 -0400

    add a test of detachment

--- detachment_velo.py
+++ detachment_velo.py
@@ -6,5 +6,3 @@ costal_file = 'tri/ll_costal.txt'
 surface_list = [Costal(costal_file).plot(), Fault('detachment').tri("tot_slip")]
 
 SetUp(surface_list).reg("detachment_slip", 'RdBu_r', 'total slip')
-
-

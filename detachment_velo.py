eb43975550682f4779c9a3b6806aa5584323b320
Evan-PC-chiang <113721214+Evan-PC-chiang@users.noreply.github.com>
2023-10-23 18:25:27 -0400

    a gigantic update

--- detachment_velo.py
+++
@@ -1,8 +0,0 @@
-from PlotFault import Fault, Costal, SetUp
-import plotly.io as pio
-pio.renderers.default = "browser"
-
-costal_file = 'tri/ll_costal.txt'
-surface_list = [Costal(costal_file).plot(), Fault('detachment').tri("tot_slip")]
-
-SetUp(surface_list).reg("detachment_slip", 'RdBu_r', 'total slip')
--- single_fault.py
+++
@@ -1,5 +0,0 @@
-from PlotFault import Fault, Costal, SetUp
-import numpy as np
-import plotly.graph_objects as go
-
-
